10a4c3ef26e6bd631e60718e1d997f7a0e1d665a
ride90 <olegpshenichniy@gmail.com>
2020-01-23 23:09:38 +0100

    Ability to install with dev requirements

--- setup.py
+++ setup.py
@@ -3,6 +3,16 @@ from setuptools import find_packages, setup
 with open('README.md', 'r') as fh:
     long_description = fh.read()
 
+requirements = (
+    'Django>=1.11,<4.0',
+    'djangorestframework>=3.0,<4.0',
+    'setuptools',
+)
+
+dev_requirements = (
+    'pytest',
+)
+
 setup(
     name='django-requestlogs',
     zip_safe=False,
@@ -17,9 +27,8 @@ setup(
     url='https://github.com/Raekkeri/django-requestlogs',
     download_url='https://github.com/raekkeri/django-requestlogs/tarball/0.1',
     packages=find_packages(exclude=['tests']),
-    install_requires=[
-        'Django>=1.11,<4.0',
-        'djangorestframework>=3.0,<4.0',
-        'setuptools',
-    ],
+    install_requires=requirements,
+    extras_require={
+        'dev': dev_requirements
+    },
 )
