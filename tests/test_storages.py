275c879605199e7583a64ffee84e8255978e4028
Teemu Husso <teemu.husso@gmail.com>
2021-02-15 19:54:43 +0200

    Fix JsonDumpField with uploaded binary file

--- tests/test_storages.py
+++ tests/test_storages.py
@@ -1,17 +1,34 @@
+from io import BytesIO
+from unittest.mock import patch
+
 import django
 from django.urls import reverse_lazy
-from django.test import override_settings, TestCase
+from django.test import override_settings, modify_settings, TestCase
 if django.VERSION[0] < 2:
     from django.conf.urls import url
 else:
     from django.urls import re_path as url
 from rest_framework import serializers
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+from rest_framework.test import APITestCase
 
 from requestlogs.storages import JsonDumpField, BaseStorage
 
 
+@api_view(['POST'])
+def upload_view(request):
+    class FileSerializer(serializers.Serializer):
+        file = serializers.FileField()
+
+    s = FileSerializer(data=request.data)
+    assert s.is_valid()
+    return Response({'status': 'ok'})
+
+
 urlpatterns = [
     url(r'^/?$', lambda r: None, name='home'),
+    url(r'^upload/?$', upload_view),
 ]
 
 
@@ -26,3 +43,20 @@ class TestSerializeProxyObject(TestCase):
         storage = SimpleStorage()
         s = storage.prepare({'blob': {'url': reverse_lazy('home')}})
         assert s == {'blob': '{"url": "/"}'}
+
+
+@override_settings(
+    ROOT_URLCONF=__name__,
+    REQUESTLOGS={'STORAGE_CLASS': 'tests.test_views.TestStorage'},
+)
+@modify_settings(MIDDLEWARE={
+    'append': 'requestlogs.middleware.RequestLogsMiddleware',
+})
+class TestWithFileUpload(APITestCase):
+    def test_post_file(self):
+        buf = BytesIO(b'\x89PNG')
+        with patch('tests.test_views.TestStorage.do_store') as mocked_store:
+            response = self.client.post('/upload', data={'file': buf})
+
+        assert mocked_store.call_args[0][0]['request']['data'] == \
+            '{"file": "<InMemoryUploadedFile, size=4>"}'
